62c391e674f173aca638b2afca12da39c6a092e3
Wrenard Guevara <guevarawrenard@gmail.com>
2023-03-16 01:48:53 -0700

    first commit

--- src/page/Footer.tsx
+++ src/page/Footer.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
-import Map from "./components/Map"
+import Facebooklogo from "./components/Facebooklogo";
+import Linkedinlogo from "./components/Linkedinlogo";
+import Map from "./components/Map";
+import Twitterlogo from "./components/Twitterlogo";
 
 function Footer() {
   return (
     <>
       <BrowserRouter>
         <div className="flex flex-col">
-
-      <Map/>
+          <Map />
 
           <div className="bg-black text-white font-poppins text-center pt-16 pb-10 sm:px-44 sm:justify-center">
             <p className="text-yellow-500 font-crimsontext text-5xl pb-4">
@@ -50,62 +52,13 @@ function Footer() {
                 </li>
               </div>
             </ul>
+
             <div className="flex flex-row justify-center gap-6 items-center py-6">
-              <svg
-                version="1.2"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 7 13"
-                width="7"
-                height="13"
-              >
-                <defs>
-                  <image
-                    width="7"
-                    height="13"
-                    id="img4"
-                    href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAcAAAANCAMAAABSF4SHAAAAAXNSR0IB2cksfwAAAGZQTFRF2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUgdCtPDAAAACJ0Uk5TAE/Q8faUD/+5M7hLLRlaZ7zWrqNMmb1/LAxjMnw2hi/ydlOv21MAAABHSURBVHicbck1AoAwAMXQoB8v7lLuf0nanSxvCBCEUZxAKlcGuVSUFdRSA5hW6npD6LcGxkmalxU2aXefQzr/vKTb+1j7wgea5wOH3hSuAAAAAABJRU5ErkJggg=="
-                  />
-                </defs>
-                <style></style>
-                <use href="#img4" x="0" y="0" />
-              </svg>
-              <svg
-                version="1.2"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 13 10"
-                width="13"
-                height="10"
-              >
-                <defs>
-                  <image
-                    width="13"
-                    height="10"
-                    id="img5"
-                    href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA0AAAAKCAMAAABYMCT2AAAAAXNSR0IB2cksfwAAAIdQTFRF2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUggdwpkAAAAC10Uk5TAAIVnjgXlS7K/80nA8uXJO5mz8aBJRr+hIePH7f8ThExUR6c6lRTodbjtG8OUOgZTAAAAGFJREFUeJwtiu0aQmAQRLe8YooKEer1HaL7vz67m/mxe84zQ3QgydEx+t2Tx9fH+SIWhLjSDcBdywiIEzY8UrbsiT25lMUu5Uu3bxXLZKq6+VvL1vVWefjojMZp/i7rT3ADFx4FhEky2DwAAAAASUVORK5CYII="
-                  />
-                </defs>
-                <style></style>
-                <use href="#img5" x="0" y="0" />
-              </svg>
-              <svg
-                version="1.2"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 13 13"
-                width="13"
-                height="13"
-              >
-                <defs>
-                  <image
-                    width="13"
-                    height="13"
-                    id="img6"
-                    href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA0AAAANCAMAAABFNRROAAAAAXNSR0IB2cksfwAAAJZQTFRF2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUgvnIzMwAAADJ0Uk5TSq5pAP8kvNwRBn6OjUmqpi950sdl7GDL6Gv3ZIdX1PVEuvRsTsDybela3ZC2pLU3iKyYJqq0AAAAbUlEQVR4nE3MWQ6CUBQE0atdiOCsqCgqDjg/p/1vjgQCWH8n6bRZq606A7xGHfxuIwX6W4a9vgbD0XgynUWyOSikbGFLYq1gnWzAr7SVdqSV9tKBY6ETZNKZS6FrrVv5eZceYE/3UubeH+nrfjnoOwgRaxO+NwAAAABJRU5ErkJggg=="
-                  />
-                </defs>
-                <style></style>
-                <use href="#img6" x="0" y="0" />
-              </svg>
+              <Facebooklogo />
+              <Twitterlogo />
+              <Linkedinlogo />
             </div>
+
             <p className="text-xs">Barbers &copy; 2018. All Rights Reserved.</p>
           </div>
         </div>
---
+++ src/page/components/Facebooklogo.tsx
@@ -0,0 +1,26 @@
+function Facebooklogo() {
+  return (
+    <div>
+      <svg
+        version="1.2"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 7 13"
+        width="7"
+        height="13"
+      >
+        <defs>
+          <image
+            width="7"
+            height="13"
+            id="img4"
+            href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAcAAAANCAMAAABSF4SHAAAAAXNSR0IB2cksfwAAAGZQTFRF2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUg2qUgdCtPDAAAACJ0Uk5TAE/Q8faUD/+5M7hLLRlaZ7zWrqNMmb1/LAxjMnw2hi/ydlOv21MAAABHSURBVHicbck1AoAwAMXQoB8v7lLuf0nanSxvCBCEUZxAKlcGuVSUFdRSA5hW6npD6LcGxkmalxU2aXefQzr/vKTb+1j7wgea5wOH3hSuAAAAAABJRU5ErkJggg=="
+          />
+        </defs>
+        <style></style>
+        <use href="#img4" x="0" y="0" />
+      </svg>
+    </div>
+  );
+}
+
+export default Facebooklogo;
