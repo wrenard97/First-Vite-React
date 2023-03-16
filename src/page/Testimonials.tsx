a07d934ca89688a5286d1a995a93a6c43d1de45b
Wrenard Guevara <guevarawrenard@gmail.com>
2023-03-16 01:38:14 -0700

    first commit

--- src/page/Testimonials.tsx
+++ src/page/Testimonials.tsx
@@ -1,4 +1,5 @@
 import bgtest from "../images/background-1.png";
+import Ellipse from "./components/Ellipse";
 
 function Testimonials() {
   return (
@@ -21,36 +22,7 @@ function Testimonials() {
         <p className="">John Doe, CEO Microsoft</p>
         <p className="pb-8">July 17, 2018</p>
         <div className="flex flex-row">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="12px"
-            viewBox="0 0 24 24"
-            width="12px"
-            fill="#d9a520"
-          >
-            <path d="M24 24H0V0h24v24z" fill="none" />
-            <circle cx="12" cy="12" r="8" />
-          </svg>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="12px"
-            viewBox="0 0 24 24"
-            width="12px"
-            fill="#d9a520"
-          >
-            <path d="M0 0h24v24H0V0z" fill="none" />
-            <path d="M12 6c3.31 0 6 2.69 6 6s-2.69 6-6 6-6-2.69-6-6 2.69-6 6-6m0-2c-4.42 0-8 3.58-8 8s3.58 8 8 8 8-3.58 8-8-3.58-8-8-8z" />
-          </svg>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="12px"
-            viewBox="0 0 24 24"
-            width="12px"
-            fill="#d9a520"
-          >
-            <path d="M0 0h24v24H0V0z" fill="none" />
-            <path d="M12 6c3.31 0 6 2.69 6 6s-2.69 6-6 6-6-2.69-6-6 2.69-6 6-6m0-2c-4.42 0-8 3.58-8 8s3.58 8 8 8 8-3.58 8-8-3.58-8-8-8z" />
-          </svg>
+          <Ellipse />
         </div>
       </div>
     </div>
